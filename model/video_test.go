15a2d862015d4be469bb0ef60197b974279e884c
wujunli <260434607@qq.com>
2023-09-02 17:20:16 +0800

    [test]新增video模型测试

---
+++ model/video_test.go
@@ -0,0 +1,55 @@
+package model
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestVideoDao_InsertNewVideo(t *testing.T) {
+	var expected error = nil
+	res := NewVideoDao().InsertNewVideo(123, 456, 100, 200,
+		"www.baidu123.com", "www.baidu345.com", "baidu123")
+	fmt.Println("res", res)
+	assert.Equal(t, expected, res)
+}
+
+func TestVideoDao_InsertNewVideo2(t *testing.T) {
+	var expected error = nil
+	res := NewVideoDao().InsertNewVideo(345, 678, 300, 400,
+		"www.baidu345.com", "www.baidu678.com", "baidu456")
+	fmt.Println("res", res)
+	assert.Equal(t, expected, res)
+}
+
+func TestVideoDao_QueryVideo(t *testing.T) {
+	expectedtitle := "baidu123"
+	video, err := NewVideoDao().QueryVideo(123)
+	if err != nil {
+		fmt.Println("the video", expectedtitle, "not found")
+		return
+	}
+	assert.Equal(t, expectedtitle, video.Title)
+
+}
+
+func TestVideoDao_UpdateVideo(t *testing.T) {
+	err := NewVideoDao().UpdateVideo(123, 456, 100, 200,
+		"www.wy123.com", "www.wy456.com", "wy123")
+	fmt.Println("err:", err)
+
+}
+
+func TestVideoDao_UpdateVideo2(t *testing.T) {
+
+}
+
+func TestVideoDao_DeleteVideo(t *testing.T) {
+	err := NewVideoDao().DeleteVideo(345)
+	assert.Equal(t, nil, err)
+}
+
+func TestVideoDao_DeleteDeletedVideo(t *testing.T) {
+	err := NewVideoDao().DeleteDeletedVideo(345)
+	assert.Equal(t, nil, err)
+}
