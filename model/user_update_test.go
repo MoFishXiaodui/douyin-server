bcb25de101f1255b24683152605a0d52cee22afe
leisure12 <2609908876@qq.com>
2023-09-04 18:04:33 +0800

    [test] User表Update函数新增测试案例

--- model/user_update_test.go
+++ model/user_update_test.go
@@ -8,12 +8,24 @@ func TestUpdate(t *testing.T) {
 		t.Errorf("sth wrong happened when init MySQL: %v", err)
 	}
 	test_user := &User{Name: "daoge", WorkCount: 10}
-	expect1 := NewUserDaoInstance().Update(1, test_user)
-	expect2 := NewUserDaoInstance().Update(11, test_user)
-	if expect1 != Success {
-		t.Errorf("Expected %v do not match actual %v", expect1, Success)
+	real1 := NewUserDaoInstance().Update(1, test_user)
+	real2 := NewUserDaoInstance().Update(11, test_user)
+
+	test_user2 := &User{WorkCount: 7}
+	real3 := NewUserDaoInstance().Update(1, test_user2)
+	if real1 != Success {
+		t.Errorf("Expected %v do not match actual %v", Success, real1)
 	}
-	if expect2 != Fail {
-		t.Errorf("Expected %v do not match actual %v", expect2, Fail)
+	if real2 != Fail {
+		t.Errorf("Expected %v do not match actual %v", Fail, real2)
+	}
+	if real3 != Success {
+		t.Errorf("Expected %v do not match actual %v", Success, real3)
+	}
+
+	test_user3 := &User{Name: "小刀", WorkCount: 7}
+	real4 := NewUserDaoInstance().Update(1, test_user3)
+	if real4 != Fail {
+		t.Errorf("Expected %v do not match actual %v", Fail, real4)
 	}
 }
