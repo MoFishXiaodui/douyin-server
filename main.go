3937a8dbd2fb1d706bb9892804bebf47c3f30eaa
Snooze <likegongcheng@foxmail.com>
2023-09-13 02:17:19 +0800

    [style] 添加部分路由注释

--- main.go
+++ main.go
@@ -18,12 +18,14 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	r.POST("/token_analysis/", router.TokenAnalysisRoute) // token 解析测试
+
 	// 基础接口
-	r.GET("/douyin/feed/", router.DouyinFeed)
-	r.POST("/douyin/user/login/", router.DouyinUserLogin)
-	r.POST("/token_analysis/", router.TokenAnalysisRoute)
-	r.GET("/douyin/user/", middleware.UserAuth, router.DouyinUser)
-	r.POST("/douyin/publish/action/", middleware.UserAuth, router.DouyinPublishAction)
+	r.GET("/douyin/feed/", router.DouyinFeed)                                          // 视频流
+	r.POST("/douyin/user/login/", router.DouyinUserLogin)                              // 用户登录
+	r.GET("/douyin/user/", middleware.UserAuth, router.DouyinUser)                     // 用户信息
+	r.POST("/douyin/publish/action/", middleware.UserAuth, router.DouyinPublishAction) // 投稿
 
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
