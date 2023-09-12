e93d8e54ff46865a9fa3148a394db376e9dc6093
Snooze <likegongcheng@foxmail.com>
2023-09-12 16:36:33 +0800

    [feat] 登录路由新增token签发 & [feat] 新增用户认证中间件

--- controller/login_test.go
+++ controller/login_test.go
@@ -11,8 +11,8 @@ func TestUserQueryLogin(t *testing.T) {
 		t.Errorf("sth wrong happened when init MySQL: %v", err)
 	}
 
-	loginData := ControllerUserQueryLogin("刀哥", "123")
-	expected := &LoginData{
+	loginData := UserLoginPost("刀哥", "123")
+	expected := &UserLoginRsp{
 		StatusCode: 0,
 		StatusMsg:  "success",
 		Token:      "gg",
@@ -21,8 +21,8 @@ func TestUserQueryLogin(t *testing.T) {
 		t.Errorf("something wrong happened")
 	}
 
-	loginData1 := ControllerUserQueryLogin("刀哥", "456")
-	expected1 := &LoginData{
+	loginData1 := UserLoginPost("刀哥", "456")
+	expected1 := &UserLoginRsp{
 		StatusCode: -1,
 		StatusMsg:  "failure",
 		Token:      "failure",
--- router/douyin_user_login.go
+++ router/douyin_user_login.go
@@ -3,10 +3,20 @@ package router
 import (
 	"dy/controller"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // 接收一个HTTP请求
 func DouyinUserLogin(c *gin.Context) {
 	data := controller.UserLoginPost(c.Query("username"), c.Query("password"))
-	c.JSON(200, data)
+	if data.StatusCode == -1 {
+		c.JSON(http.StatusForbidden, data)
+	} else if data.StatusCode == 0 {
+		c.JSON(http.StatusOK, data)
+	} else {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status_msg":  "unknown err",
+			"status_code": "-2",
+		})
+	}
 }
